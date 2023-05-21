2be29bfd77625793f80f932ed63101d33eee288e
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-05-21 22:46:45 +0600

    first commit

---
+++ static_data.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x=10;
+    
+    return 0;
+}
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int x=10/0;
+//     cout << "HEY!\n";
+//     cout << x << endl; // runtime error
+//     return 0;
+// }
